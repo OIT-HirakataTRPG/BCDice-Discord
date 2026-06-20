fabef4a3d2f0942a455ffa1b43b6d1506e74781f
yakinikusun <yakinikudaisuki01@gmail.com>
2026-06-20 14:06:40 +0000

    add: Tableへの挿入

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits} = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { DynamicLoader } = require('bcdice');
 const dotenv = require('dotenv');
 const Database = require('better-sqlite3');
 
-const db = new Database('./db/setting.db');
 dotenv.config();
 
 const client = new Client({
@@ -16,23 +15,26 @@ const client = new Client({
 	]
 });
 
-
 client.commands = new Collection();
 
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
-for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		// Set a new item in the Collection with the key as the command name and the value as the exported module
-		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
-		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
+const db = new Database('./db/setting.db', { verbose: console.log });
+
+db.prepare(`CREATE TABLE IF NOT EXISTS DiceSystem (
+  user_id INTEGER PRIMARY KEY,
+  system TEXT NOT NULL
+);`).run();
+
+
+const commandsPath = path.join(__dirname, 'commands/utility');
+const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 	}
 }
 
