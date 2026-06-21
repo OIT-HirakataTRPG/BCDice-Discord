d956baa64e55d26e1dc0442783598d54a38fe9e7
yakinikusun <yakinikudaisuki01@gmail.com>
2026-06-21 15:36:26 +0000

    fix: helpの表記

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -3,25 +3,26 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const Database = require('better-sqlite3');
 
 const dicehelpEmbed = new EmbedBuilder()
-    .setTitle('DiceBotについて')
+    .setTitle('このBotについて')
     .setColor(0x336699)
 
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('help')
-        .setDescription('ダイスの詳細を表示します'),
+        .setDescription('ヘルプを表示します'),
 
     async execute(interaction) {
-        dicehelpEmbed.setFields({ name: 'このBotについて', value: 'BCDiceを利用したDiscord用のダイスボットです' });
-        dicehelpEmbed.addFields({ name: '\`/dice\`', value: 'ダイスを振ることができます' , inline: true });
-        dicehelpEmbed.addFields({ name: '\`/d\`', value: '\`/dice\`の短縮版です' });
-        dicehelpEmbed.addFields({ name: '\`/setdice\`', value: 'ダイスの種類を変更します' , inline: true });
-        dicehelpEmbed.addFields({ name: '\`/info\`', value: 'ダイスの詳細を表示します' , inline: true });
+        dicehelpEmbed.setDescription('BCDiceを利用したDiscord用のダイスボットです');
+        dicehelpEmbed.addFields({ name: '\`/dice\`', value: 'ダイスを振ることができます', inline: true });
+        dicehelpEmbed.addFields({ name: '\`/d\`', value: '\`/dice\`の短縮版です', inline: true});
+        dicehelpEmbed.addFields({ name: '', value: ''});
+        dicehelpEmbed.addFields({ name: '\`/setdice\`', value: 'ダイスの種類を変更します', inline: true});
+        dicehelpEmbed.addFields({ name: '\`/info\`', value: 'ダイスの詳細を表示します', inline: true });
         await interaction.reply({
             embeds: [dicehelpEmbed],
             allowedMentions: { repliedUser: false },
-            flags: 'Ephemeral'
+            flags: 'SuppressNotifications'
         });
     }
 };
