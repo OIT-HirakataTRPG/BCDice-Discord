6f337ed8bf858a56a204a05962d4ad0523740ac7
yakinikusun <yakinikudaisuki01@gmail.com>
2026-06-21 12:34:40 +0000

    fix: Embedの内容が増えるのを修正

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -1,7 +1,7 @@
 const { DynamicLoader } = require('bcdice');
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const Database = require('better-sqlite3');
-const {translateSystemName} = require('../helper/translate_system_name');
+const { translateSystemName } = require('../helper/translate_system_name');
 
 const db = new Database('./db/setting.db');
 const getUser = db.prepare(`SELECT * FROM DiceSystem WHERE user_id = ?`);
@@ -25,16 +25,20 @@ module.exports = {
         const GameSystem = await loader.dynamicLoad(system);
         const helpText = GameSystem.HELP_MESSAGE;
         if (system !== 'DiceBot') {
-            dicehelpEmbed.addFields({ name: `${await translateSystemName(system)} ダイス説明`, value:`>>> ${helpText.replace(/\*/g, '\\*')}` });
+            dicehelpEmbed.setFields({ name: `${await translateSystemName(system)} ダイス説明`, value: `>>> ${helpText.replace(/\*/g, '\\*')}` });
             const DiceBot = await loader.dynamicLoad("DiceBot");
             const DiceHelpBase = DiceBot.HELP_MESSAGE.replace("詳細は下記URLのコマンドガイドを参照\nhttps://docs.bcdice.org/\n", "");
             dicehelpEmbed.addFields({ name: '共通ダイス', value: `>>> ${DiceHelpBase.replace(/^(.+)/gm, `- $1`)}` });
             dicehelpEmbed.addFields({ name: '詳細は下記URLのコマンドガイドを参照', value: 'https://docs.bcdice.org/' });
         } else {
-            dicehelpEmbed.addFields({ name: '共通ダイス', value: `>>> ${helpText.replace("詳細は下記URLのコマンドガイドを参照\nhttps://docs.bcdice.org/\n", "").replace(/^(.+)/gm, `- $1`)}` });
+            dicehelpEmbed.setFields({ name: '共通ダイス', value: `>>> ${helpText.replace("詳細は下記URLのコマンドガイドを参照\nhttps://docs.bcdice.org/\n", "").replace(/^(.+)/gm, `- $1`)}` });
             dicehelpEmbed.addFields({ name: '詳細は下記URLのコマンドガイドを参照', value: 'https://docs.bcdice.org/' });
         }
-        await interaction.reply({ embeds: [dicehelpEmbed], allowedMentions: { repliedUser: false }, flags: 'Ephemeral' });
+        await interaction.reply({
+            embeds: [dicehelpEmbed],
+            allowedMentions: { repliedUser: false },
+            flags: 'Ephemeral'
+        });
     }
 };
 
