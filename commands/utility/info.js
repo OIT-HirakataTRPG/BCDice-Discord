b23f9ec0770c907fe145f6d65953899ce70f0699
yakinikusun <yakinikudaisuki01@gmail.com>
2026-06-30 16:09:35 +0000

    change: /infoをComponentsV2に

--- commands/utility/info.js
+++ commands/utility/info.js
@@ -1,14 +1,20 @@
 const { DynamicLoader } = require("bcdice");
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const {
+	SlashCommandBuilder,
+	MessageFlags,
+	EmbedBuilder,
+	ContainerBuilder,
+	TextDisplayBuilder,
+	SectionBuilder,
+} = require("discord.js");
 const Database = require("better-sqlite3");
 const { translateSystemName } = require("../helper/translate_system_name");
 
 const db = new Database("./db/setting.db");
 const getUser = db.prepare(`SELECT * FROM DiceSystem WHERE user_id = ?`);
 
-const dicehelpEmbed = new EmbedBuilder()
-	// .setTitle('DiceBotについて')
-	.setColor(0x336699);
+const dicehelpEmbed = new EmbedBuilder();
+// .setTitle('DiceBotについて')
 
 module.exports = {
 	data: new SlashCommandBuilder().setName("info").setDescription("ダイスの詳細を表示します"),
@@ -19,37 +25,37 @@ module.exports = {
 		const loader = new DynamicLoader();
 		const GameSystem = await loader.dynamicLoad(system);
 		const helpText = GameSystem.HELP_MESSAGE;
-		if (["ShinobiGami", "KyokoShinshoku", "SwordWorld2.5"].includes(system)) {
-			await interaction.reply({
-				content: `>>> ### ${await translateSystemName(system)} ダイス説明\n${helpText.replace(/\*/g, "\\*")}`,
-				allowedMentions: { repliedUser: false },
-				flags: "Ephemeral",
-			});
-			return;
-		}
+		let Component = new ContainerBuilder().setAccentColor(0x336699);
+		// if (["ShinobiGami", "KyokoShinshoku", "SwordWorld2.5"].includes(system)) {
+		// 	await interaction.reply({
+		// 		content: `>>> ### ${await translateSystemName(system)} ダイス説明\n${helpText.replace(/\*/g, "\\*")}`,
+		// 		allowedMentions: { repliedUser: false },
+		// 		flags: "Ephemeral",
+		// 	});
+		// 	return;
+		// }
 		if (system !== "DiceBot") {
-			dicehelpEmbed.setFields({
-				name: `${await translateSystemName(system)} ダイス説明`,
-				value: `>>> ${helpText.replace(/\*/g, "\\*")}`,
-			});
-			dicehelpEmbed.addFields({
-				name: "共通ダイスの詳細は下記URLのコマンドガイドを参照",
-				value: "https://docs.bcdice.org/",
-			});
+			const systemName = await translateSystemName(system);
+			Component.addTextDisplayComponents((textDisplay) =>
+				textDisplay.setContent(
+					`### ${systemName} ダイス説明\n>>> ${helpText.replace(/\*/g, "\\*")}`,
+				),
+			).addTextDisplayComponents((textDisplay) =>
+				textDisplay.setContent(`共通ダイスの詳細は下記URLのコマンドガイドを参照\nhttps://docs.bcdice.org/`),
+			);
 		} else {
-			dicehelpEmbed.setFields({
-				name: "共通ダイス",
-				value: `>>> ${helpText.replace("詳細は下記URLのコマンドガイドを参照\nhttps://docs.bcdice.org/\n", "").replace(/^(.+)/gm, `- $1`)}`,
-			});
-			dicehelpEmbed.addFields({
-				name: "詳細は下記URLのコマンドガイドを参照",
-				value: "https://docs.bcdice.org/",
-			});
+			Component.addTextDisplayComponents((textDisplay) =>
+				textDisplay.setContent(
+					`### 共通ダイス\n>>> ${helpText.replace("詳細は下記URLのコマンドガイドを参照\nhttps://docs.bcdice.org/\n", "").replace(/^(.+)/gm, `- $1`)}`,
+				),
+			).addTextDisplayComponents((textDisplay) =>
+				textDisplay.setContent(`詳細は下記URLのコマンドガイドを参照\nhttps://docs.bcdice.org/`),
+			);
 		}
 		await interaction.reply({
-			embeds: [dicehelpEmbed],
+			components: [Component],
 			allowedMentions: { repliedUser: false },
-			flags: "Ephemeral",
+			flags: [MessageFlags.IsComponentsV2, MessageFlags.Ephemeral],
 		});
 	},
 };
