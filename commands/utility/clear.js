ea2be85292d403b8fc957a15f3072bcfdfb29799
yakinikusun <yakinikudaisuki01@gmail.com>
2026-06-30 13:48:47 +0000

    format形式をPritterに変更

--- commands/utility/clear.js
+++ commands/utility/clear.js
@@ -5,36 +5,37 @@ const db = new Database("./db/setting.db", { verbose: console.log });
 db.pragma("journal_mode = WAL");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('clear')
-        .setDescription('DBを初期化します'),
+	data: new SlashCommandBuilder().setName("clear").setDescription("DBを初期化します"),
 
-     async execute(interaction) {
-        if (!interaction.inGuild()) {
-            await interaction.reply({
-                content: "DMではこのコマンドを実行できません",
-                allowedMentions: { repliedUser: false },
-                flags: 'Ephemeral'
-            });
-            return;
-        }
-        
-        if (interaction.guild.id !== process.env.ADMINGUILDID || !interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
-            await interaction.reply({
-                content: "このコマンドを実行するには管理者権限が必要です",
-                allowedMentions: { repliedUser: false },
-                flags: 'Ephemeral'
-            });
-            return;
-        }
+	async execute(interaction) {
+		if (!interaction.inGuild()) {
+			await interaction.reply({
+				content: "DMではこのコマンドを実行できません",
+				allowedMentions: { repliedUser: false },
+				flags: "Ephemeral",
+			});
+			return;
+		}
 
-        const clearDB = db.prepare(`DELETE FROM DiceSystem;`);
-        clearDB.run();
+		if (
+			interaction.guild.id !== process.env.ADMINGUILDID ||
+			!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)
+		) {
+			await interaction.reply({
+				content: "このコマンドを実行するには管理者権限が必要です",
+				allowedMentions: { repliedUser: false },
+				flags: "Ephemeral",
+			});
+			return;
+		}
 
-        await interaction.reply({
-            content: "DBを初期化しました",
-            allowedMentions: { repliedUser: false },
-            flags: 'Ephemeral'
-        });
-     }
+		const clearDB = db.prepare(`DELETE FROM DiceSystem;`);
+		clearDB.run();
+
+		await interaction.reply({
+			content: "DBを初期化しました",
+			allowedMentions: { repliedUser: false },
+			flags: "Ephemeral",
+		});
+	},
 };
--- commands/utility/stopbot.js
+++ commands/utility/stopbot.js
@@ -1,33 +1,34 @@
 const { SlashCommandBuilder, PermissionsBitField } = require("discord.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('stopbot')
-        .setDescription('Botを停止します'),
+	data: new SlashCommandBuilder().setName("stopbot").setDescription("Botを停止します"),
 
-    async execute(interaction) {
-        if (!interaction.inGuild()) {
-            await interaction.reply({
-                content: "DMではこのコマンドを実行できません",
-                allowedMentions: { repliedUser: false },
-                flags: 'Ephemeral'
-            });
-            return;
-        }
+	async execute(interaction) {
+		if (!interaction.inGuild()) {
+			await interaction.reply({
+				content: "DMではこのコマンドを実行できません",
+				allowedMentions: { repliedUser: false },
+				flags: "Ephemeral",
+			});
+			return;
+		}
 
-        if (interaction.guild.id !== process.env.ADMINGUILDID || !interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
-            await interaction.reply({
-                content: "このコマンドを実行するには管理者権限が必要です",
-                allowedMentions: { repliedUser: false },
-                flags: 'Ephemeral'
-            });
-            return;
-        }
+		if (
+			interaction.guild.id !== process.env.ADMINGUILDID ||
+			!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)
+		) {
+			await interaction.reply({
+				content: "このコマンドを実行するには管理者権限が必要です",
+				allowedMentions: { repliedUser: false },
+				flags: "Ephemeral",
+			});
+			return;
+		}
 
-        await interaction.reply({
-            content: `\# ⚠情報漏洩厳禁⚠\nDocURL: ${process.env.DOCURL}`,
-            allowedMentions: { repliedUser: false },
-            flags: 'Ephemeral'
-        });
-    }
+		await interaction.reply({
+			content: `\# ⚠情報漏洩厳禁⚠\nDocURL: ${process.env.DOCURL}`,
+			allowedMentions: { repliedUser: false },
+			flags: "Ephemeral",
+		});
+	},
 };
